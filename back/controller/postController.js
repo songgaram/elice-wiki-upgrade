a091bb32ee43864cb92f91f358715d397b4215fe
nowgnas <marshmello.sw@gmail.com>
2022-04-13 01:50:37 +0900

    fix : post 생성 요청 시 body와 date 처리 부분 수정
    - date는 postService에서 현재 시간 받아서 처리
    - body는 텍스트 받는 것으로 변경

--- back/controller/postController.js
+++ back/controller/postController.js
@@ -4,16 +4,23 @@ class postController {
   static async addPost(req, res, next) {
     try {
       // 요청으로부터 데이터 받아오기
-      const { userId, date, week, tag, title, body } = req.body;
+      const { userId, week, tag, title } = req.body;
+      // body에 text가 저장되어 올 것
+      // TODO: 텍스트를 어떻게 받아와야 잘 저장이 될지 고민..
+      // 아래 방법으로는 md파일에 저장해도 제대로 보여주지 못함
+      // front에서 바로 md파일을 만들 수 있는지 찾아보기, 백에서도 동일하게
+      const body = `#title 
+      ## h2 
+      -p tag`;
+
       const newPost = await postService.addPost({
         userId,
-        date,
         week,
         tag,
-        title,
         body,
+        title,
       });
-      res.status(201).json(newPost).end();
+      res.status(201).json(body).end();
     } catch (error) {
       console.log(error.message);
     }
--- back/services/postService.js
+++ back/services/postService.js
@@ -1,14 +1,48 @@
 import { postModel } from "../db/models/post/post";
 import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
 
 class postService {
   // 값 req에서 받아와 추가 하기
   // tag 여러개 가져오는 방법은 프론트와 함께 얘기해봐야 함 #tag 이런식??
   // date는 createdAt을 사용해도 괜찮지 않을까..
-  static async addPost({ userId, date, week, tag, title, body }) {
+  static async addPost({ userId, week, tag, title, body }) {
+    // body에서 받은 text를 md파일로 저장
+    // TODO: const savePath = '../../front/post' -> 저장하게될 예상 경로
+    const nowDate = new Date();
+    const year = nowDate.getUTCFullYear();
+
+    // 월과 일은 2자리가 아닌경우 앞에 0을 붙여줌
+    const month =
+      nowDate.getUTCMonth().toString().length !== 2
+        ? `0${nowDate.getUTCMonth()}`
+        : nowDate.getUTCMonth();
+
+    const day =
+      nowDate.getUTCDate().toString().length !== 2
+        ? `0${nowDate.getUTCDate()}`
+        : nowDate.getUTCDate();
+
+    const date = `${year}.${month}.${day}`;
+    console.log(date);
+
     const postId = uuidv4();
 
-    const newPost = { postId, userId, date, week, tag, title, body };
+    // 테스트 경로
+    const savePath = "../_post";
+    // postId는 라우팅 경로로 사용될 수 있으므로 shortId로 만드는 것도 괜찮을 듯
+    fs.writeFile(
+      `${year}-${month}-${day}-${postId}.md`,
+      "\ufeff" + body,
+      {
+        encoding: "utf-8",
+      },
+      (err, data) => {
+        console.log(data);
+      }
+    );
+
+    const newPost = { postId, userId, date, week, tag, title };
     const insertedPost = await postModel
       .insertPost({ newPost })
       .then(() => console.log("post created"))
