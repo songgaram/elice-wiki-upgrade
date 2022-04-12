8cfbc61caabe170b3007729bfc742a0aa0a23d5c
songgaram <pager1436@gmail.com>
2022-04-12 22:45:13 +0900

    feat : storeCreator로 store 생성 / 미들웨어로 thunk 사용
    - store/store.js
    - redux-thunk 설치

---
+++ front/src/store/store.js
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from "redux";
+import userReducer from "./reducers/userReducer";
+import ReduxThunk from "redux-thunk";
+
+const store = createStore(userReducer, applyMiddleware(ReduxThunk));
+
+export default store;
