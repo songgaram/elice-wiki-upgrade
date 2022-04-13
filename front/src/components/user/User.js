8972139a54738e0e9ddbaa8963bc9521d091ff4f
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-14 02:13:17 +0900

    Feat : user css 적용

---
+++ front/src/components/user/User.js
@@ -0,0 +1,67 @@
+import styled from "../../styles/user.module.css";
+
+const User = () => {
+    const user = {
+        id: "70f9e61f-092f-438b-a268-c8c38369a0a3",
+        name: "KIM HYUNSEO",
+        description: "NodeJS Developer",
+        email: "khs050305@naver.com",
+        image: "https://avatars.githubusercontent.com/u/74223263?v=4",
+        likes: [],
+        createdAt: "2022-03-26T09:39:51.049+00:00",
+        updatedAt: "2022-03-26T09:41:22.622+00:00",
+        __v: 0,
+    };
+
+    const posts = [
+        {
+            id: "1",
+            user_id: "70f9e61f-092f-438b-a268-c8c38369a0a3",
+            title: "Recoil로 어플리케이션 다크모드 구현하기",
+            description: "개인 프로젝트 다크모드를 구현하던 도중 문득 Recoil로 기능을 구현하고 싶었다.",
+            tags: ["CSS싫어요", "리액트"],
+            createdAt: "2022-04-11T09:39:51.049+00:00",
+            updatedAt: "2022-04-11T09:41:22.622+00:00",
+            __v: 0,
+        },
+        {
+            id: "2",
+            user_id: "70f9e61f-092f-438b-a218-c8c38369a0a3",
+            title: "Recoil로 어플리케이션 다크모드 구현하기",
+            description: "개인 프로젝트 다크모드를 구현하던 도중 문득 Recoil로 기능을 구현하고 싶었다.",
+            tags: ["CSS싫어요", "리액트"],
+            createdAt: "2022-04-11T09:39:51.049+00:00",
+            updatedAt: "2022-04-11T09:41:22.622+00:00",
+            __v: 0,
+        },
+        {
+            id: "3",
+            user_id: "70f9261f-092f-438b-a268-c8c38369a0a3",
+            title: "Recoil로 어플리케이션 다크모드 구현하기",
+            description: "개인 프로젝트 다크모드를 구현하던 도중 문득 Recoil로 기능을 구현하고 싶었다.",
+            tags: ["CSS싫어요", "리액트"],
+            createdAt: "2022-04-11T09:39:51.049+00:00",
+            updatedAt: "2022-04-11T09:41:22.622+00:00",
+            __v: 0,
+        },
+    ];
+    return (
+        <>
+            <article className={styled.user_container}></article>
+            <article className={styled.post_container}>
+                {posts.map((posts) => (
+                    <div className={styled.post}>
+                        {posts.tags.map((tags) => (
+                            <button className={styled.post_tag}>{tags}</button>
+                        ))}
+                        <h1 className={styled.post_title}>{posts.title}</h1>
+                        <h4 className={styled.post_description}>{posts.description}</h4>
+                        <span className={styled.post_date}>{posts.createdAt.split("T")[0].replaceAll("-", ".")}</span>
+                    </div>
+                ))}
+            </article>
+        </>
+    );
+};
+
+export default User;
