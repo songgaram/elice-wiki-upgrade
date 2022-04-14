6eb3037beeeaa7395a221051b2dbfeb6a19cf648
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-15 01:16:07 +0900

    Feat : Admin 페이지 라우팅, User 페이지 useEffect로 API 호출

--- front/src/App.js
+++ front/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import User from "./components/user/User";
+import Admin from "./components/admin/Admin";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/users/:userId" element={<User />} />
-                <Route path="*" element={<User />} />
+                <Route path="/users/:userId/admin" element={<User />} />
+                <Route path="*" element={<Admin />} />
             </Routes>
         </Router>
     );
