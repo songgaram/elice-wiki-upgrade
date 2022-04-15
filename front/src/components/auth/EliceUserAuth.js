a64f5312892c1b51562a09a9b14153756df641a9
songgaram <pager1436@gmail.com>
2022-04-15 18:40:36 +0900

    feat : EliceUserAuth 컴포넌트 내용 수정

--- front/src/components/auth/EliceUserAuth.js
+++ front/src/components/auth/EliceUserAuth.js
@@ -1,1 +1,28 @@
-const EliceUserAuth = () => {};
+import { InputGroup, FormControl, Button } from "react-bootstrap";
+
+const EliceUserAuth = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img src="/image/logo_large.png" style={{ width: "10%" }} />
+      <img src="/image/cal-bot.png" style={{ width: "30%" }} />
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <Button variant="outline-secondary" id="button-addon2">
+          Button
+        </Button>
+      </InputGroup>
+    </div>
+  );
+};
+export default EliceUserAuth;
