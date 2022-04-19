8f8f0e9785887a88b2cd6c8b572063f368391b00
KA <song.kyunga@gmail.com>
2022-04-19 23:17:11 +0900

    feat: db에 comment.js 추가

---
+++ back/db/migrations/20220419141650-create-comment.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      commentId: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.STRING
+      },
+      boardId: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comments');
+  }
+};
