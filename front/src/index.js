a1015286420f7e8552c2c72940b78f8bffc3fc5e
songgaram <pager1436@gmail.com>
2022-05-10 17:56:32 +0900

    feat : index.js index 경로 변경

--- front/src/index.js
+++ front/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import store from "./store/store";
+import store from "./store/index";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById("root")
 );
