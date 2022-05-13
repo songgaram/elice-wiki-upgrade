b770800d6a00cfef906faa1dc6c7ec93a4512403
songgaram <pager1436@gmail.com>
2022-05-13 23:19:11 +0900

    style : module.css => styled-components 수정

--- front/src/components/view/home/Home.js
+++ front/src/components/view/home/Home.js
@@ -1,21 +1,29 @@
-import Button from "@mui/material/Button";
-import Divider from "@mui/material/Divider";
-
-import styles from "./Home.module.css";
+import { Button, Divider } from "@mui/material";
+import styled from "styled-components";
 
 const Home = () => {
+    const handleClick = () => {};
+
     return (
         <>
-            <div className={styles.home_container}>
+            <div style={{ minHeight: "100vh", height: "auto" }}>
                 <header style={{ height: "60px" }} />
-                <div className={styles.home_header}>
+                <WeekNav>
                     <Button disabled style={{ color: "black" }}>
                         WEEK
                     </Button>
 
                     {new Array(24).fill(null).map((_, week) => (
                         <>
-                            <Button sx={{ minWidth: "45px" }} variant="text">
+                            <Button
+                                sx={{
+                                    minWidth: "2.5%",
+                                    maxHeight: "50%",
+                                    borderRadius: "30px",
+                                    margin: "0 3px",
+                                }}
+                                onClick={handleClick}
+                            >
                                 {String(week + 1).padStart(2, "0")}
                             </Button>
                             <Divider
@@ -26,37 +34,60 @@ const Home = () => {
                         </>
                     ))}
 
-                    <Button color="primary">기타</Button>
-                </div>
-                <div className={styles.contents_container}>
-                    <div className={styles.home_side}></div>
-                    <div className={styles.home_contents}>
+                    <Button
+                        color="primary"
+                        sx={{
+                            minWidth: "2.5%",
+                            maxHeight: "50%",
+                            borderRadius: "30px",
+                            margin: "0 3px",
+                        }}
+                    >
+                        기타
+                    </Button>
+                </WeekNav>
+                <Container>
+                    <ContentsSide />
+                    <Contents>
                         <div>
                             <div>tag</div>
                             <div>title</div>
                         </div>
-                        <div>
-                            <div>tag</div>
-                            <div>title</div>
-                        </div>
-                        <div>
-                            <div>tag</div>
-                            <div>title</div>
-                        </div>{" "}
-                        <div>
-                            <div>tag</div>
-                            <div>title</div>
-                        </div>
-                        <div>
-                            <div>tag</div>
-                            <div>title</div>
-                        </div>
-                    </div>
-                    <div className={styles.home_side}></div>
-                </div>
+                    </Contents>
+                    <ContentsSide />
+                </Container>
             </div>
         </>
     );
 };
+
+const WeekNav = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    background-color: #f1f1f1;
+`;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: calc(100vh - 100px);
+    background-color: #e1e1e1;
+`;
+
+const ContentsSide = styled.div`
+    width: 25%;
+    background-color: white;
+`;
+
+const Contents = styled.div`
+    width: 50%;
+    background-color: #f1f1f1;
+    overflow: scroll;
+`;
 
 export default Home;
