add0ddb7803d54db904cc249d7dd04bab081c44f
hongloans <loans93@gmail.com>
2022-05-17 16:55:36 +0900

    modify: app use order modification

--- back/src/app.js
+++ back/src/app.js
@@ -13,9 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(adminRouter);
 app.use(userRouter);
 app.use(authRouter);
-app.use(adminRouter);
 app.use(postRouter);
 app.use(tagRouter);
 
