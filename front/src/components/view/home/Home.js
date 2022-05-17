81cfcc8e26ea8404260d7397c64bdf0d27f9f902
songgaram <pager1436@gmail.com>
2022-05-17 14:20:37 +0900

    refact : WeekList 컴포넌트 Home에서 분리

--- front/src/components/view/home/Home.js
+++ front/src/components/view/home/Home.js
@@ -1,65 +1,13 @@
-import { useState } from "react";
-import { Button, Divider } from "@mui/material";
 import styled from "styled-components";
 import PostList from "./PostList";
+import WeekList from "./WeekList";
 
 const Home = () => {
-    const [isClicked, setIsClicked] = useState(new Array(25).fill(false));
-    const handleClick = (e) => {
-        const newArr = new Array(25).fill(false);
-        newArr[e.target.value] = !newArr[e.target.value];
-        setIsClicked(newArr);
-    };
-
     return (
         <>
             <div style={{ minHeight: "100vh", height: "auto" }}>
                 <header style={{ height: "60px" }} />
-                <WeekNav>
-                    <Button disabled style={{ color: "black" }}>
-                        WEEK
-                    </Button>
-
-                    {new Array(24).fill(null).map((_, idx) => (
-                        <>
-                            <Button
-                                value={idx}
-                                key={`week_${idx}`}
-                                size="small"
-                                variant={isClicked[idx] ? "contained" : "text"}
-                                sx={{
-                                    minWidth: "2.5%",
-                                    maxHeight: "50%",
-                                    borderRadius: "30px",
-                                    margin: "0 3px",
-                                }}
-                                onClick={handleClick}
-                            >
-                                {String(idx + 1).padStart(2, "0")}
-                            </Button>
-                            <Divider
-                                orientation="vertical"
-                                variant="middle"
-                                flexItem
-                            />
-                        </>
-                    ))}
-
-                    <Button
-                        value={24}
-                        variant={isClicked[24] ? "contained" : "text"}
-                        size="small"
-                        sx={{
-                            minWidth: "3%",
-                            maxHeight: "50%",
-                            borderRadius: "30px",
-                            margin: "0 3px",
-                        }}
-                        onClick={handleClick}
-                    >
-                        기타
-                    </Button>
-                </WeekNav>
+                <WeekList />
                 <Container>
                     <ContentsSide />
                     <Contents>
@@ -71,16 +19,6 @@ const Home = () => {
         </>
     );
 };
-
-const WeekNav = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    height: 40px;
-    background-color: #f1f1f1;
-`;
 
 const Container = styled.div`
     display: flex;
--- front/src/components/view/home/WeekList.js
+++ front/src/components/view/home/WeekList.js
@@ -1,7 +1,74 @@
-import styles from "./WeekList.module.css";
+import { useState } from "react";
+import { Button, Divider } from "@mui/material";
+import styled from "styled-components";
 
 const WeekList = () => {
-    return;
+    const [isClicked, setIsClicked] = useState(new Array(25).fill(false));
+    const handleClick = (e) => {
+        const newArr = new Array(25).fill(false);
+        newArr[e.target.value] = !newArr[e.target.value];
+        setIsClicked(newArr);
+    };
+
+    return (
+        <>
+            <WeekNav>
+                <Button disabled style={{ color: "black" }}>
+                    WEEK
+                </Button>
+
+                {new Array(24).fill(null).map((_, idx) => (
+                    <>
+                        <Button
+                            value={idx}
+                            key={`week_${idx}`}
+                            size="small"
+                            variant={isClicked[idx] ? "contained" : "text"}
+                            sx={{
+                                minWidth: "2.5%",
+                                maxHeight: "50%",
+                                borderRadius: "30px",
+                                margin: "0 3px",
+                            }}
+                            onClick={handleClick}
+                        >
+                            {String(idx + 1).padStart(2, "0")}
+                        </Button>
+                        <Divider
+                            orientation="vertical"
+                            variant="middle"
+                            flexItem
+                        />
+                    </>
+                ))}
+
+                <Button
+                    value={24}
+                    variant={isClicked[24] ? "contained" : "text"}
+                    size="small"
+                    sx={{
+                        minWidth: "3%",
+                        maxHeight: "50%",
+                        borderRadius: "30px",
+                        margin: "0 3px",
+                    }}
+                    onClick={handleClick}
+                >
+                    기타
+                </Button>
+            </WeekNav>
+        </>
+    );
 };
+
+const WeekNav = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    background-color: #f1f1f1;
+`;
 
 export default WeekList;
