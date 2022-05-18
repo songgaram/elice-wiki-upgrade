8f620f033f4d61602d1d0ff2f7150fd32a99e349
songgaram <pager1436@gmail.com>
2022-05-18 15:45:24 +0900

    feat : intro 브랜치 병합 / 버튼 컴포넌트 연결
    - feature-intro-front + feature-login-front
    - OAuthBtn 구글 로그인 기능 연결

--- front/src/components/view/home/Home.js
+++ front/src/components/view/home/Home.js
@@ -1,13 +1,14 @@
 import OAuthButton from "../../auth/OAuthButton";
 import { useSelector } from "react-redux";
 import UserHome from "./UserHome";
+import Intro from "../Intro/Intro";
 
 const Home = () => {
     const userState = useSelector((state) =>
         state ? state.userReducer.user : undefined
     );
 
-    return <>{userState?.authorized ? <UserHome /> : <OAuthButton />}</>;
+    return <>{userState?.authorized ? <UserHome /> : <Intro />}</>;
 };
 
 export default Home;
--- front/src/components/view/home/UserHome.js
+++ front/src/components/view/home/UserHome.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../../store/actions/userAction";
 
 function UserHome() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const userState = useSelector((state) =>
         state ? state.userReducer.user : undefined
     );
     const userAuthorized = userState?.authorized;
     const [isFetchCompleted, setIsFetchCompleted] = useState(false);
+
+    const handleLogout = () => {
+        // dispatch 함수를 이용해 로그아웃함.
+        dispatch(logoutUser());
+        // 기본 페이지로 돌아감.
+        navigate("/");
+    };
 
     useEffect(() => {
         if (!userAuthorized) {
@@ -22,7 +32,12 @@ function UserHome() {
     if (!isFetchCompleted) {
         return <div>로딩중</div>;
     }
-    return <div>안녕 나는 유저홈</div>;
+    return (
+        <>
+            <button onClick={handleLogout}>로그아웃</button>
+            <div>안녕 나는 유저홈</div>;
+        </>
+    );
 }
 
 export default UserHome;
