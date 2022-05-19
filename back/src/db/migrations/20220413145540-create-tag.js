33107d46d6b6f99223d53300640708f5dfe42b4d
songgaram <pager1436@gmail.com>
2022-05-19 10:38:14 +0900

    feat : Tags => Tag

--- back/src/db/migrations/20220413145540-create-tag.js
+++ back/src/db/migrations/20220413145540-create-tag.js
@@ -23,6 +23,6 @@ module.exports = {
         });
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable("Tags");
+        await queryInterface.dropTable("Tag");
     },
 };
