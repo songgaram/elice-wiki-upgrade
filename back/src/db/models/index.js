fd6861d7577a884f33b9f2ad3982331147c6955a
songgaram <pager1436@gmail.com>
2022-05-20 14:12:33 +0900

    fix : board back model/index.js 수정
    board back, front merge 완료

--- back/src/db/models/index.js
+++ back/src/db/models/index.js
@@ -10,37 +10,39 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config, {
-    logging: console.log,
-  });
+    sequelize = new Sequelize(process.env[config.use_env_variable], config, {
+        logging: console.log,
+    });
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config,
-    { logging: console.log }
-  );
+    sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config,
+        { logging: console.log }
+    );
 }
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
+    .filter((file) => {
+        return (
+            file.indexOf(".") !== 0 &&
+            file !== basename &&
+            file.slice(-3) === ".js"
+        );
+    })
+    .forEach((file) => {
+        const model = require(path.join(__dirname, file))(
+            sequelize,
+            Sequelize.DataTypes
+        );
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 const Users = require("./user")(sequelize, Sequelize.DataTypes);
