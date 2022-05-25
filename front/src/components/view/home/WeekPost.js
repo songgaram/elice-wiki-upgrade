93a295bc8bd58a9aa699712b87b12123e6fd4b92
songgaram <pager1436@gmail.com>
2022-05-25 10:23:24 +0900

    test : react-intersectioi-observer test

--- front/src/components/view/home/WeekPost.js
+++ front/src/components/view/home/WeekPost.js
@@ -1,5 +1,6 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useCallback } from "react";
 // import { getWeekPosts } from "./HomeData";
+import { useInView } from "react-intersection-observer";
 import { useParams } from "react-router-dom";
 import Loader from "../../Loader";
 import { Button } from "@mui/material";
@@ -9,21 +10,20 @@ import * as Api from "../../../api";
 
 function WeekPost() {
     const [posts, setPosts] = useState([]);
-    const [goal, setGoal] = useState(undefined);
-    const [observing, setObserving] = useState(false);
+    // const [goal, setGoal] = useState(undefined);
+    // const [observing, setObserving] = useState(false);
     const [page, setPage] = useState(1);
     const [totalPage, setTotalPage] = useState(undefined);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [isFetchCompleted, setIsFetchCompleted] = useState(false);
-    const target = useRef();
+    const [loading, setLoading] = useState(false);
+    const [ref, inView] = useInView();
+    // const [isFetchCompleted, setIsFetchCompleted] = useState(false);
+
     const params = useParams();
     const week = params.week;
 
     //     const weekState = useSelector((state) =>
     //     state ? state.weekReducer : undefined
     // );
-
-    let num = 1;
 
     // const fetchSetState = (data) => {
     //     setPosts((prev) => [...prev, ...data.payload.postListInfo]);
@@ -32,29 +32,31 @@ function WeekPost() {
     //     setIsLoaded(false);
     // };
 
-    const loadMore = () => {
-        setPage((curr) => curr + 1);
-    };
-
-    const getWeekPosts = async () => {
+    const getWeekPosts = useCallback(async () => {
         try {
+            setLoading(true);
             const { data } = await Api.getQuery(
                 `post/week/${week}`,
                 `page=${page}&perPage=10`
             );
             setPosts(data.payload.postListInfo);
-            setObserving(true);
-            setTotalPage(data.payload.totalPage);
-            setIsLoaded(false);
+            setLoading(false);
             // isFetchCompleted(true);
         } catch (e) {
             console.log("Week-Post를 가져오는데 실패하였습니다.", e);
         }
-    };
+    }, [page]);
 
     useEffect(() => {
         getWeekPosts();
-    }, [page, week]);
+    }, [getWeekPosts, week]);
+
+    useEffect(() => {
+        // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
+        if (inView && !loading) {
+            setPage((prevState) => prevState + 1);
+        }
+    }, [inView, loading]);
 
     // useEffect(() => {
     //     if (observing) {
@@ -82,7 +84,18 @@ function WeekPost() {
     return (
         <>
             {posts.map((post, idx) => (
-                <Post key={`post_${idx}`} post={post} idx={idx} />
+                <>
+                    {post.length - 1 == idx ? (
+                        <Post
+                            key={`post_${idx}`}
+                            post={post}
+                            idx={idx}
+                            ref={ref}
+                        />
+                    ) : (
+                        <Post key={`post_${idx}`} post={post} idx={idx} />
+                    )}
+                </>
             ))}
             {/* <TargetElement ref={target}>{isLoaded && <Loader />}</TargetElement> */}
         </>
