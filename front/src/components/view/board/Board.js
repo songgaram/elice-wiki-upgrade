c3a87618abd86d85acfc46ef72baa8df76445091
songgaram <pager1436@gmail.com>
2022-05-26 16:03:28 +0900

    feat : header 추가

--- front/src/components/view/board/Board.js
+++ front/src/components/view/board/Board.js
@@ -4,8 +4,6 @@ import {
   List,
   ListSubheader,
   Box,
-  Container,
-  CssBaseline,
   Table,
   TableBody,
   TableCell,
@@ -18,6 +16,7 @@ import {
 import BorderColorIcon from "@mui/icons-material/BorderColor";
 import Spinner from "../../Spinner";
 import Header from "../Header";
+import styled from "styled-components";
 import * as Api from "../../../api";
 
 function Board() {
@@ -46,78 +45,87 @@ function Board() {
   return (
     <>
       <Header />
-      <React.Fragment>
-        <CssBaseline />
-        <Container maxWidth="md">
-          <Box
+      <Container>
+        <Box
+          sx={{
+            minWidth: "70%",
+            height: "auto",
+            marginTop: "1%",
+          }}
+        >
+          <List
             sx={{
               width: "100%",
-              height: "100vh",
-              marginTop: "60px",
+              bgcolor: "background.paper",
             }}
+            aria-labelledby="nested-list-subheader"
           >
-            <List
-              sx={{
+            <ListSubheader
+              component="div"
+              id="nested-list-subheader"
+              style={{
+                textAlign: "center",
                 width: "100%",
-                bgcolor: "background.paper",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
               }}
-              aria-labelledby="nested-list-subheader"
             >
-              <ListSubheader
-                component="div"
-                id="nested-list-subheader"
-                style={{
-                  textAlign: "center",
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-              >
-                <span style={{ fontSize: "1rem", fontWeight: "700" }}>자유게시판</span>
-                <IconButton onClick={() => navigate("/board/create")}>
-                  <BorderColorIcon />
-                </IconButton>
-              </ListSubheader>
-            </List>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>번호</TableCell>
-                    <TableCell align="center">제목</TableCell>
-                    <TableCell align="center">작성자</TableCell>
-                    <TableCell align="right">날짜</TableCell>
+              <span style={{ fontSize: "1rem", fontWeight: "700" }}>자유게시판</span>
+              <IconButton onClick={() => navigate("/board/create")}>
+                <BorderColorIcon />
+              </IconButton>
+            </ListSubheader>
+          </List>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>번호</TableCell>
+
+                  <TableCell align="center">제목</TableCell>
+                  <TableCell align="center">작성자</TableCell>
+                  <TableCell align="right">날짜</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {boardList?.map((board) => (
+                  <TableRow
+                    key={board.id}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                    onClick={() => navigate(`/board/${board.boardId}`)}
+                    style={{ cursor: "pointer" }}
+                    hover={true}
+                  >
+                    <TableCell component="th" scope="row">
+                      {board.id}
+                    </TableCell>
+
+                    <TableCell align="center">
+                      [{board.header}]{board.title}
+                    </TableCell>
+                    <TableCell align="center">{board.userName}</TableCell>
+                    <TableCell align="right">{board.createdAt.slice(0, 10)}</TableCell>
                   </TableRow>
-                </TableHead>
-                <TableBody>
-                  {boardList?.map((board) => (
-                    <TableRow
-                      key={board.id}
-                      sx={{
-                        "&:last-child td, &:last-child th": { border: 0 },
-                      }}
-                      onClick={() => navigate(`/board/${board.boardId}`)}
-                      style={{ cursor: "pointer" }}
-                      hover={true}
-                    >
-                      <TableCell component="th" scope="row">
-                        {board.id}
-                      </TableCell>
-                      <TableCell align="center">{board.title}</TableCell>
-                      <TableCell align="center">{board.userName}</TableCell>
-                      <TableCell align="right">{board.createdAt.slice(0, 10)}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Box>
-        </Container>
-      </React.Fragment>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  width: 100vw;
+  height: cal(100vh - 64px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default Board;
--- front/src/components/view/board/BoardContents.js
+++ front/src/components/view/board/BoardContents.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import {
-    Card,
-    CardHeader,
-    CardActions,
-    CardContent,
-    Typography,
-    IconButton,
-    Button,
+  Card,
+  CardHeader,
+  CardActions,
+  CardContent,
+  Typography,
+  IconButton,
+  Button,
 } from "@mui/material/";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -15,64 +15,57 @@ import DeleteIcon from "@mui/icons-material/Delete";
 // import Confirm from "../Confirm";
 
 function BoardContents({ setIsEditing, isEditable, handleDelete, boardData }) {
-    const navigate = useNavigate();
-    const { title, content, createdAt, author } = boardData;
-    const [open, setOpen] = useState(false);
-    const confirmTitle = "게시글을 삭제하실 건가요?";
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const navigate = useNavigate();
+  const { title, content, createdAt, author, header } = boardData;
+  const [open, setOpen] = useState(false);
+  const confirmTitle = "게시글을 삭제하실 건가요?";
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <>
-            <Card sx={{ minWidth: 275 }}>
-                <CardHeader
-                    action={
-                        <Button size="small" onClick={() => navigate("/board")}>
-                            목록
-                        </Button>
-                    }
-                    title={title}
-                    subheader={`${author} | ${createdAt.slice(0, 10)}`}
-                />
-                <CardContent>
-                    <Typography
-                        sx={{ fontSize: 14 }}
-                        color="text.secondary"
-                        gutterBottom
-                    >
-                        {content}
-                    </Typography>
-                </CardContent>
+  return (
+    <>
+      <Card sx={{ minWidth: 275 }}>
+        <CardHeader
+          action={
+            <Button size="small" onClick={() => navigate("/board")}>
+              목록
+            </Button>
+          }
+          title={`[${header}]${title}`}
+          subheader={`${author} | ${createdAt.slice(0, 10)}`}
+        />
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {content}
+          </Typography>
+        </CardContent>
 
-                <CardActions disableSpacing>
-                    {isEditable && (
-                        <>
-                            <IconButton
-                                onClick={() => setIsEditing(true)}
-                                sx={{ marginLeft: "auto" }}
-                            >
-                                <EditIcon />
-                            </IconButton>
-                            <IconButton onClick={() => handleClickOpen()}>
-                                <DeleteIcon />
-                            </IconButton>
-                            {/* <Confirm
+        <CardActions disableSpacing>
+          {isEditable && (
+            <>
+              <IconButton onClick={() => setIsEditing(true)} sx={{ marginLeft: "auto" }}>
+                <EditIcon />
+              </IconButton>
+              <IconButton onClick={() => handleClickOpen()}>
+                <DeleteIcon />
+              </IconButton>
+              {/* <Confirm
                                 open={open}
                                 handleClose={handleClose}
                                 handleDelete={handleDelete}
                                 title={confirmTitle}
                             /> */}
-                        </>
-                    )}
-                </CardActions>
-            </Card>
-        </>
-    );
+            </>
+          )}
+        </CardActions>
+      </Card>
+    </>
+  );
 }
 
 export default BoardContents;
