5a939ee99c06a4bd403eb43d2b66ba932a110e60
hongloans <loans93@gmail.com>
2022-05-27 23:50:29 +0900

    fix: bug fix

--- front/src/components/auth/GoogleLoading.js
+++ front/src/components/auth/GoogleLoading.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../store/actions/userAction";
 import Spinner from "../Spinner";
-import { useEffect } from "react";
 
 const GoogleLoading = () => {
     const navigate = useNavigate();
