5bd1869c89d509ec10f75c056b5f725d929f9044
songgaram <pager1436@gmail.com>
2022-07-11 16:11:43 +0900

    chore : react-query install / provider code 수정

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "./store/actions/userAction";
 import Api from "libs/api";
+
 import Home from "view/home/Home";
 import Note from "view/note/Note";
 import UserHome from "view/home/UserHome";
@@ -22,23 +23,8 @@ import Board from "view/board/Board";
 import BoardDetail from "view/board/BoardDetail";
 import BoardAddForm from "view/board/BoardAddForm";
 import Spinner from "components/Spinner";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Mdfile from "view/note/Markdown";
 import PostEditForm from "view/note/PostEditForm";
-import GlobalStyle from "./GlobalStyle";
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#7353EA",
-            darker: "#322468",
-        },
-        binary: {
-            main: "#757575",
-            darker: "#3A3A3A",
-        },
-    },
-});
 
 function App() {
     const dispatch = useDispatch();
@@ -68,45 +54,42 @@ function App() {
     }
 
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <Router>
-                <Routes>
-                    <Route path="/" exact element={<Home />} />
-                    {userState && <Route path="/auth" exact element={<EliceUserAuth />} />}
-                    {userState?.authorized && (
-                        <>
-                            <Route path="/home" exact element={<UserHome />}>
-                                <Route index element={<HomePost />} />
-                                <Route path="post" element={<HomePost />} />
-                                <Route path="post/:postId" element={<Mdfile />} />
-                                <Route path="week/:week" element={<WeekPost />} />
-                                <Route path="tag/:tag" element={<TagPost />} />
-                            </Route>
-                            <Route path="/addPost" element={<Note />} />
-                            <Route path="/editPost" element={<PostEditForm />} />
-                            <Route path="/board" exact element={<Board />} />
-                            <Route path="/board/:id" exact element={<BoardDetail />} />
-                            <Route path="/board/create" exact element={<BoardAddForm />} />
-                        </>
-                    )}
-                    <Route path="/test" exact element={<GoogleLoading />} />
-                    <Route path="*" element={<Home />} />
-                    {(userState?.admin === 0 || userState?.admin === 1) && (
-                        <Route path="/admin" element={<Admin />}>
-                            <Route path="board" element={<ManageBoard />} />
-                            <Route path="posts" element={<ManagePosts />} />
-                            <Route path="users" element={<ManageUsers />} />
-                            <Route path="questions" element={<ManageQuestions />} />
+        <Router>
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                {userState && <Route path="/auth" exact element={<EliceUserAuth />} />}
+                {userState?.authorized && (
+                    <>
+                        <Route path="/home" exact element={<UserHome />}>
+                            <Route index element={<HomePost />} />
+                            <Route path="post" element={<HomePost />} />
+                            <Route path="post/:postId" element={<Mdfile />} />
+                            <Route path="week/:week" element={<WeekPost />} />
+                            <Route path="tag/:tag" element={<TagPost />} />
                         </Route>
-                    )}
-                    {userState?.admin === 0 && (
-                        <Route path="editquestion/:id" element={<QuestionEditor />} />
-                    )}
-                    <Route path="mypage" element={<MyPage />} />
-                </Routes>
-            </Router>
-        </ThemeProvider>
+                        <Route path="/addPost" element={<Note />} />
+                        <Route path="/editPost" element={<PostEditForm />} />
+                        <Route path="/board" exact element={<Board />} />
+                        <Route path="/board/:id" exact element={<BoardDetail />} />
+                        <Route path="/board/create" exact element={<BoardAddForm />} />
+                    </>
+                )}
+                <Route path="/test" exact element={<GoogleLoading />} />
+                <Route path="*" element={<Home />} />
+                {(userState?.admin === 0 || userState?.admin === 1) && (
+                    <Route path="/admin" element={<Admin />}>
+                        <Route path="board" element={<ManageBoard />} />
+                        <Route path="posts" element={<ManagePosts />} />
+                        <Route path="users" element={<ManageUsers />} />
+                        <Route path="questions" element={<ManageQuestions />} />
+                    </Route>
+                )}
+                {userState?.admin === 0 && (
+                    <Route path="editquestion/:id" element={<QuestionEditor />} />
+                )}
+                <Route path="mypage" element={<MyPage />} />
+            </Routes>
+        </Router>
     );
 }
 
