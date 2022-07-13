25f122b8f71d2ad398a377a07e1a1483bc8a39ba
songgaram <pager1436@gmail.com>
2022-07-13 13:14:58 +0900

    fix : userCurrent query retry 횟수 변경/auth post code 수정
    - invalidate code remove

--- front/src/queries/userQuery.js
+++ front/src/queries/userQuery.js
@@ -10,6 +10,7 @@ export function useGetCurrentUser() {
             return { userState: res.data };
         },
         {
+            retry: 1,
             staleTime: Infinity,
             onError: () => console.log("%c sessionStorage에 토큰 없음.", "color: #d93d1a;"),
         },
@@ -18,12 +19,7 @@ export function useGetCurrentUser() {
 
 // auth 정답을 post함
 export const usePostAuthAnswer = () => {
-    const queryClient = useQueryClient();
-
     return useMutation(async (answer) => await Api.post("user/auth", answer), {
-        onSuccess: () => {
-            queryClient.invalidateQueries("userState");
-        },
         onError: (err) => console.log("인증 실패ㅠㅠ", err),
     });
 };
