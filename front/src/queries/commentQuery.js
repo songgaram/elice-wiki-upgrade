7b3178ef52816e7aacd22c1ee926fd967e492c08
songgaram <pager1436@gmail.com>
2022-07-20 17:28:59 +0900

    refact : comment - post api 수정 완료

--- front/src/queries/commentQuery.js
+++ front/src/queries/commentQuery.js
@@ -1,9 +1,39 @@
-import { useQuery } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import Api from "libs/api";
 
-export const useGetCommentList = (id) => {
-    return useQuery(["comments", id], async () => {
-        const res = await Api.get(`commentlist/board/${id}`);
+export const useGetCommentList = (boardId) => {
+    return useQuery(["comments", boardId], async () => {
+        const res = await Api.get(`commentlist/board/${boardId}`);
         return res.data;
+    });
+};
+
+export const useUpdateComment = (id) => {
+    const queryClient = useQueryClient();
+    return useMutation(async (comment) => await Api.put(`comments/${id}`, comment), {
+        onSuccess: () => {
+            queryClient.invalidateQueries("comments");
+        },
+        onError: (err) => console.log("댓글 수정 실패ㅠㅠ", err),
+    });
+};
+
+export const usePostComment = () => {
+    const queryClient = useQueryClient();
+    return useMutation(async (comment) => await Api.post("comments/comment", comment), {
+        onSuccess: () => {
+            queryClient.invalidateQueries("comments");
+        },
+        onError: (err) => console.log("댓글 생성 실패ㅠㅠ", err),
+    });
+};
+
+export const useDeletecomment = (id) => {
+    const queryClient = useQueryClient();
+    return useMutation(async () => await Api.delete(`comments/${id}`), {
+        onSuccess: () => {
+            queryClient.invalidateQueries("comments");
+        },
+        onError: (err) => console.log("댓글 삭제 실패ㅠㅠ", err),
     });
 };
